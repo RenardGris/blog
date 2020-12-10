ad2c32c56b13b1a0f0ecdf0f6fb20876efbe84de
Flo <florentin-c@live.fr>
2020-12-10 13:42:42 +0100

    init core config

---
+++ core/Config.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Core;
+
+class Config
+{
+
+    private $setting = [];
+    private static $_instance;
+
+    //recupere le dossier de configuration
+    public function __construct($file)
+    {
+
+        $this->setting = require $file;
+    }
+
+    //Singleton avec le fichier config/config.php
+    public static function getInstance($file)
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new Config($file);
+        }
+
+        return self::$_instance;
+
+    }
+
+    //getter recuperant la valeur de la clé passée en parametre
+    public function get($key)
+    {
+        if (!isset($this->setting[$key])) {
+            return null;
+        }
+        return $this->setting[$key];
+    }
+
+}
