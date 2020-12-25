25f0d8cb0f485b660e10396b5deae6b1bbca8b33
Flo <florentin-c@live.fr>
2020-12-25 20:30:04 +0100

    Codacy Issues : Abstract class Session define/use/delete superglobal variable session

---
+++ core/Auth/Session.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Core\Auth;
+
+class Session
+{
+
+    public static function put($key, $value){
+        $_SESSION[$key] = $value;
+    }
+
+    public static function get($key){
+        return (isset($_SESSION[$key]) ? $_SESSION[$key] : null);
+    }
+
+    public static function forget($key){
+        unset($_SESSION[$key]);
+    }
+
+
+}
