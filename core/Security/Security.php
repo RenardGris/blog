139ae5fa06c846f626f5a4604f8d996d47aa454b
Flo <florentin-c@live.fr>
2020-12-27 20:16:09 +0100

    Codacy issues : Rework security sanitize input

--- core/Security/Security.php
+++ core/Security/Security.php
@@ -8,10 +8,15 @@ class Security
     public static function xss()
     {
         $data = [];
-        foreach($_POST as $key => $input){
-            $data[$key] = htmlentities($input);
+
+        if(filter_input_array(INPUT_POST) !== null){
+            foreach(filter_input_array(INPUT_POST) as $key => $input){
+                $cleanInput = strip_tags($input);
+                $sanitizedInput = Security::sanitizer($key,$cleanInput);
+                $data[$key] = htmlspecialchars($sanitizedInput, ENT_QUOTES);
+            }
+            return $data;
         }
-        return $data;
     }
 
     public static function csrf()
@@ -22,4 +27,19 @@ class Security
         return false;
     }
 
+    public static function sanitizer($type, $value){
+
+        $inputs = [ FILTER_SANITIZE_STRING => ['firstname','lastname','username','password','titre','chapo','contenu'],
+                    FILTER_SANITIZE_NUMBER_INT => ['role', 'article_id', 'user_id', 'id'],
+                    FILTER_SANITIZE_EMAIL => ['email']
+                ];
+
+        foreach ($inputs as $key => $genders){
+            foreach ($genders as $gender){
+                if($gender === $type){
+                    return $value = filter_var($value, $key);
+                }
+            }
+        }
+    }
 }
